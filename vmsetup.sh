11a8e53495ba827a0be09c87e2738813ade92d00
Jan Buenker <jan.buenker@grandcentrix.net>
2021-08-12 12:31:42 +0200

    Fixup setup script

--- vmsetup.sh
+++ vmsetup.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+pushd "$(dirname $0)"
+setup_dir="$(pwd)"
+popd
+
+target="$1"
+
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -10,11 +20,14 @@ sudo usermod -aG docker ${USER}
 
 bash
 
+pushd "$target"
+
 # setup hawkbit
-git clone https://github.com/eclipse/hawkbit.git
+git clone -n https://github.com/eclipse/hawkbit.git
 pushd hawkbit
 
-git apply ../compose-fixup.patch
+git checkout 2574581b2c0b1e19cc8d127409229d306616049c
+git apply "$setup_dir/compose-fixup.patch"
 
 pushd hawkbit-runtime/docker
 docker-compose up -d
@@ -23,5 +36,7 @@ popd
 popd
 
 # setup chunkstore
-pushd chunkstore
+cp -r "$setup_dir/chunkstore" "$target/chunkstore"
+pushd "$target/chunkstore"
 docker-compose up -d
+popd
